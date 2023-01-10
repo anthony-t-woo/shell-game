2073ea1fee7dc76f46aaed4c1e44409c2c45c69a
ANTHONY WOO <anthony.woo14@gmail.com>
2023-01-10 11:43:45 -0700

    Get DOM elements

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 /* Imports */
 // none for this file
 /* Get DOM Elements */
-// const shellOneEl = document.getElementById('shell-one');
-// const shellTwoEl = document.getElementById('shell-two');
-// const shellThreeEl = document.getElementById('shell-three');
+const shellOneEl = document.getElementById('shell-one');
+const shellTwoEl = document.getElementById('shell-two');
+const shellThreeEl = document.getElementById('shell-three');
+
+const shellOneButtonEl = document.getElementById('shell-one-button');
+const shellTwoButtonEl = document.getElementById('shell-two-button');
+const shellThreeButtonEl = document.getElementById('shell-three-button');
+
+const winsEl = document.getElementById('wins');
+const lossesEl = document.getElementById('losses');
+const totalEl = document.getElementById('total-guesses');
+const winPercentEl = document.getElementById('win-percent');
 
 /* State */
 
